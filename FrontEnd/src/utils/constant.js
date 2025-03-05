edc942d6832376a112946cc4436eb90d7f1d0925
Abhishek Korkora <abhishekkorkora321@gmail.com>
2025-03-05 12:14:26 +0530

    hello again

--- FrontEnd/src/utils/constant.js
+++ FrontEnd/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT = "https://job-portal-application-2-ilnb.onrender.com//v1/user";
-export const JOB_API_END_POINT="https://job-portal-application-2-ilnb.onrender.com/api/v1/job"
-export const APPLICATION_API_END_POINT="https://job-portal-application-2-ilnb.onrender.com/2/api/v1/application";
-export const COMPANY_API_END_POINT = "https://job-portal-application-2-ilnb.onrender.com//api/v1/company";
+export const USER_API_END_POINT = "https://job-portal-application-0py2.onrender.com/api/v1/user";
+export const JOB_API_END_POINT="https://job-portal-application-0py2.onrender.com/api/v1/job"
+export const APPLICATION_API_END_POINT="https://job-portal-application-0py2.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT = "https://job-portal-application-0py2.onrender.com/api/v1/company";
