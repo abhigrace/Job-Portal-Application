506edc08ed8a0db997e5e9a5c8e8c9de0833bfe9
Abhishek Korkora <abhishekkorkora321@gmail.com>
2025-03-05 11:59:09 +0530

    hello

--- BackEnd/utils/db.js
+++ BackEnd/utils/db.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/jobdetails", {
+    await mongoose.connect("mongodb+srv://abhishekkorkora321:NtximBlpThudBUr8@cluster0.fvr6v.mongodb.net/jobportal?retryWrites=true&w=majority", {
       // useNewUrlParser: true,
       // useUnifiedTopology: true,
     });
-    console.log('MongoDB (local) connected...');
+    console.log('MongoDB connected...');
   } catch (err) {
     console.error(err.message);
     process.exit(1);
